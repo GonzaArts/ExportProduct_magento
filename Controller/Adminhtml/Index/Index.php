5fee24b9c68661604b9ddf0aacd09f65c2e5b612
Gonzalo Hidalgo Madrid <gonzagamer79@gmail.com>
2024-08-18 19:34:24 +0200

    :bug: Fixed problem with access to extension functionality

---
+++ Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,26 @@
+<?php
+namespace WB\ExportProduct\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    protected $resultPageFactory;
+
+    public function __construct(
+        Action\Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('WB_ExportProduct::main_menu');
+        $resultPage->getConfig()->getTitle()->prepend(__('Export Products'));
+        return $resultPage;
+    }
+}
